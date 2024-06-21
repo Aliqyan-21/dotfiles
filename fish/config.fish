c98626cd1742abf04b466def9a0f44489e63cea8
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-06-21 22:06:07 +0530

    changes

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -g fish_greeting
     alias vim='nvim'
+    alias gts='git status'
+    alias gta='git add'
+    alias gtc='git commit'
+    alias gtp='git push'
 end
